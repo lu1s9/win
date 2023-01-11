22cc2a5289fb08cbf8b6ba6509d9440380736640
Luis Fernando <luis@lu1s9.xyz>
2023-01-11 16:42:21 -0600

    Admin view and express-validator

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,26 @@
 var express = require('express');
 var router = express.Router();
+const { body, validationResult } = require('express-validator');
 
-/* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
-});
+router.get('/', (req,res) => {
+  res.render('index')
+})
+
+router.get('/admin', (req, res) => {
+  res.render('admin')
+})
+
+router.post('/admin', 
+  body('user').exists().not().isEmpty(),
+  body('password').exists().not().isEmpty(),
+  (req,res) => {
+  const errors = validationResult(req);
+  if (!errors.isEmpty()) {
+     res.render('admin', {validaciones: errors.array()})
+  }
+  else{
+    res.send('Validacion exitosa')
+  }
+})
 
 module.exports = router;
