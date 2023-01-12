0278c4b22ab40854c22572487528b34f5c10ddb6
Luis Fernando <luis@lu1s9.xyz>
2023-01-11 19:23:47 -0600

    Database connection to save menu and dotenv package

--- controllers/controller.js
+++ controllers/controller.js
@@ -1,7 +1,22 @@
+const sqlite3 = require("sqlite3");
 const { body, validationResult } = require("express-validator");
 
+const db = new sqlite3.Database(process.env.DB, (err) => {
+  if (err) {
+    console.error(err.message);
+  }
+  console.log("Connected to database");
+});
+
 exports.index = (req, res) => {
-  res.render("index");
+  let primerTiempo, segundoTiempo, tercerTiempo;
+  db.get("SELECT * FROM menu WHERE id_menu=?", 1, (err, menu) => {
+    callback(menu);
+  });
+
+  function callback(menu) {
+    res.render("index", { menu: menu });
+  }
 };
 
 exports.admin = (req, res) => {
@@ -18,15 +33,24 @@ exports.admin_login = (req, res) => {
 };
 
 exports.admin_menu = (req, res) => {
-    res.render("menu")
-}
+  res.render("menu");
+};
 
-exports.admin_edit_menu = (req,res) => {
-    const errors = validationResult(req)
-    if (!errors.isEmpty()) {
-        console.log(errors.array())
-        res.render("menu", {validaciones: errors.array(), valores: req.body})
-    } else {
-        res.send('validacion exitosa')
-    }
-}
+exports.admin_edit_menu = (req, res) => {
+  const errors = validationResult(req);
+  if (!errors.isEmpty()) {
+    res.render("menu", { validaciones: errors.array(), valores: req.body });
+  } else {
+    const { primerTiempo, segundoTiempo, tercerTiempo } = req.body;
+    db.run(
+      "UPDATE menu SET primer_tiempo=?, segundo_tiempo=?, tercer_tiempo=? WHERE id_menu=?",
+      [
+        primerTiempo,
+        segundoTiempo,
+        tercerTiempo.split(",").map((a) => a.trim()),
+        1,
+      ]
+    );
+    res.redirect("/");
+  }
+};
